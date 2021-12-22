7046ad5d385acad952971a346161368db07cbead
vanich <vanich7@mail.ru>
2021-12-22 19:48:25 +0300

    Сделан счетчик ингридиентов

--- src/components/burger-ingredients/elem-burger-ingredients.tsx
+++ src/components/burger-ingredients/elem-burger-ingredients.tsx
@@ -1,46 +1,48 @@
 import React from 'react';
 import {
-    Counter,
-    CurrencyIcon,
+  Counter,
+  CurrencyIcon,
 } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './burger-ingredients.module.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { SET_INGRIDIENT_MODAL } from '../../services/actions/ingredients';
 import { useDrag } from 'react-dnd';
+import { RootState } from 'services/reducers';
 
 const CLASSNAMEDIV = `${styles.ingredients__items} mt-6 ml-4 mb-10 mr-4`;
 
 const ElemBurgerIngredients = (props: any): JSX.Element => {
-    const dispatch = useDispatch();
-    const [current, setCurrent] = React.useState<number>(0);
-    const [{ isDragging }, drag] = useDrag({
-        type: 'ingridient',
-        item: props,
-        collect: (monitor) => ({
-            isDragging: monitor.isDragging(),
-        }),
-    });
+  const dispatch = useDispatch();
+  const current = useSelector(
+    (store: RootState) => store.igridients.countSelectedIngredients[props._id]
+  );
+  const [{ isDragging }, drag] = useDrag({
+    type: 'ingridient',
+    item: props,
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
 
-    console.log(isDragging);
+  console.log(isDragging);
 
-    return (
-        <div
-            className={CLASSNAMEDIV}
-            onClick={() => {
-                setCurrent((prev) => ++prev);
-                dispatch({ type: SET_INGRIDIENT_MODAL, item: props });
-            }}
-            ref={drag}
-        >
-            {current ? <Counter count={current} size='default' /> : null}
-            <img className='ml-4 mr-4' src={props.image} alt={props.name} />
-            <div className={`${styles.ingredients__wrapper} mt-4 mb-4`}>
-                <p className='text text_type_main-medium mr-2'>{props.price}</p>
-                <CurrencyIcon type='primary' />
-            </div>
-            <p className='text text_type_main-default'>{props.name}</p>
-        </div>
-    );
+  return (
+    <div
+      className={CLASSNAMEDIV}
+      onClick={() => {
+        dispatch({ type: SET_INGRIDIENT_MODAL, item: props });
+      }}
+      ref={drag}
+    >
+      {current ? <Counter count={current} size='default' /> : null}
+      <img className='ml-4 mr-4' src={props.image} alt={props.name} />
+      <div className={`${styles.ingredients__wrapper} mt-4 mb-4`}>
+        <p className='text text_type_main-medium mr-2'>{props.price}</p>
+        <CurrencyIcon type='primary' />
+      </div>
+      <p className='text text_type_main-default'>{props.name}</p>
+    </div>
+  );
 };
 
 export default ElemBurgerIngredients;
