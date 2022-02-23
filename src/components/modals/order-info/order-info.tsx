4a580f51f67a7b0793cec14565a06cea49e5fdb8
vanich <vanich7@mail.ru>
2022-02-23 20:10:46 +0300

    Доработал модальное окно

--- src/components/modals/order-info/order-info.tsx
+++ src/components/modals/order-info/order-info.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-sequences */
 import { FC } from 'react';
 import { useSelector } from 'services/types';
 import { useParams } from 'react-router-dom';
@@ -5,6 +6,7 @@ import { formatTime } from 'utils/format-time';
 import { IDataProps } from 'utils/types';
 import styles from './order-info.module.css';
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { ListIngridients } from './list-ingridients';
 
 const CLASS_NAME_TIME = `text text_type_main-small text_color_inactive`;
 const CLASS_NAME_PRICE = `${styles.price} text text_type_digits-default mr-3`;
@@ -31,20 +33,43 @@ export const OrdeInfo: FC = () => {
     0
   );
 
-  const data = formatTime(createdAt);
+  const ingridientsOrder: [string, number][] = Object.entries(
+    ingredients.reduce(
+      (acum: { [key: string]: number }, id: string) => (
+        (acum[id] = (acum[id] || 0) + 1), acum
+      ),
+      {}
+    )
+  );
+
+  const timeOrder = formatTime(createdAt);
 
   return (
-    <>
+    <div className={styles.container}>
+      <p className={`text text_type_digits-default ${styles['number-order']}`}>
+        #{number}
+      </p>
       <header>
-        <h3 className='text text_type_main-medium mt-6'>{name}</h3>
+        <h3 className={`text text_type_main-medium mt-6 ${styles.title}`}>
+          {name}
+        </h3>
       </header>
-      <p className='text text_type_main-default mt-3'>{statusOrder}</p>
+      <p
+        className={`text text_type_main-default mt-3 ${styles['success-order']}`}>
+        {statusOrder}
+      </p>
       <p className='text text_type_main-medium mt-15'>Состав:</p>
+      <div>
+        <ListIngridients
+          ingridientsOrder={ingridientsOrder}
+          listIgridients={listIgridients}
+        />
+      </div>
       <footer className={styles.footer}>
-        <p className={CLASS_NAME_TIME}>{data}</p>
+        <p className={CLASS_NAME_TIME}>{timeOrder}</p>
         <p className={CLASS_NAME_PRICE}>{price}</p>
         <CurrencyIcon type='primary' />
       </footer>
-    </>
+    </div>
   );
 };
