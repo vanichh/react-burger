a3335b1337aacd1fd5bda121155e1fd441ec6447
Ivan <vanich7@mail.ru>
2022-04-15 14:25:53 +0300

    Доработка функции

--- src/utils/format-time.ts
+++ src/utils/format-time.ts
@@ -1,28 +1,34 @@
+const transliteTime = (
+  number: number,
+  arrName: [string, string, string] = ['день', 'дней', 'дня']
+): string => {
+  switch (number) {
+    case 0:
+      return 'Сегодня ';
+    case 1:
+      return 'Вчера ';
+    default:
+      const newNumber = number % 10;
+      if (newNumber > 10 && newNumber < 20) {
+        return `${number} ${arrName[2]} назад`;
+      } else if (newNumber > 1 && newNumber < 5) {
+        return `${number} ${arrName[1]} назад`;
+      } else if (newNumber === 1) {
+        return `${number} ${arrName[0]} назад`;
+      }
+      return `${number} ${arrName[2]} назад`;
+  }
+};
+
 export const formatTime = (time: string): string => {
-  let stringData: string = '';
   const date = new Date(time);
   const dataUnix = Math.round(date.getTime() / 1000);
   const currentDataUnix = Math.round(new Date().getTime() / 1000);
   const differenceday = Math.round((currentDataUnix - dataUnix) / 86400);
-  switch (differenceday) {
-    case 0: {
-      stringData += 'Сегодня ';
-      break;
-    }
-    case 1: {
-      stringData += 'Вчера ';
-      break;
-    }
-    default: {
-      stringData += `${differenceday} дней назад `;
-    }
-  }
+  const dayOrder = transliteTime(differenceday);
   const hourAndMinute = Intl.DateTimeFormat('ru', {
     timeStyle: 'short',
   }).format(date);
-  stringData += hourAndMinute;
   const timeZpne = ` I-GTM${Math.floor(date.getTimezoneOffset() / 60)}`;
-  stringData += timeZpne;
-
-  return stringData;
+  return `${dayOrder} ${hourAndMinute} ${timeZpne} `;
 };
