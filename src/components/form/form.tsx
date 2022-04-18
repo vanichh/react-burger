f76d3e9dc07999c83ca1f700f592f89cfed67d03
Ivan <vanich7@mail.ru>
2022-04-18 15:24:30 +0300

    Доработка функции

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -4,15 +4,16 @@ import styles from './form.module.css';
 interface IForm {
   children: React.ReactNode;
   onSubmit?: (e: FormEvent) => void;
+  className?: string;
 }
 
-export const Form: FC<IForm> = ({ children, onSubmit }) => {
+export const Form: FC<IForm> = ({ children, onSubmit, className }) => {
   return (
-    <form onSubmit={onSubmit} className={styles.aligin_center_form}>
+    <form
+      onSubmit={onSubmit}
+      className={className || styles.aligin_center_form}
+    >
       {children}
     </form>
   );
 };
-
-
-
