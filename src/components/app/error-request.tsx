fffd8b86590b19a21f10aaf24de955da4553beed
Ivan <vanich7@mail.ru>
2022-04-18 15:01:29 +0300

    Исправление стилей

--- src/components/app/error-request.tsx
+++ src/components/app/error-request.tsx
@@ -1,16 +1,11 @@
 import { FC } from 'react';
 import { useSelector } from 'services/types/';
+import styles from './app.module.css';
 
 export const ErrorRequest: FC = () => {
   const { errorRequestText } = useSelector((store) => store.igridients);
   return (
-    <div
-      style={{
-        flexDirection: 'column',
-        display: 'flex',
-        alignItems: 'center',
-      }}
-    >
+    <div className={styles.error}>
       <p className='text text_type_main-medium mt-10 mb-10'>
         Произошла ошибка космического маштаба
       </p>
