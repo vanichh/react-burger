01024c84200092d5591e3807983112cb0a5c9a08
Ivan <vanich7@mail.ru>
2022-04-25 15:27:09 +0300

    Доработка кода и добавление тени

--- src/components/order-list/list-img-Igridients.tsx
+++ src/components/order-list/list-img-Igridients.tsx
@@ -1,46 +1,38 @@
 import { FC, ReactNode } from 'react';
 import { IDataProps } from 'utils/types';
 import styles from './order-list.module.css';
+import cn from 'classnames';
 
 interface IListImgIgridients {
   ArrIdIngredients: string[];
   listIgridients: IDataProps[];
   children?: ReactNode;
 }
-
-let counterIdIngredients: null | number = null;
-
 export const ListImgIgridients: FC<IListImgIgridients> = (props) => {
   const { ArrIdIngredients, listIgridients } = props;
 
-  let ArrIgridients = ArrIdIngredients.map((id) =>
+  const ArrIgridients = ArrIdIngredients.map((id) =>
     listIgridients.find(({ _id }) => _id === id)
   );
-
-  if (ArrIgridients.length > 6) {
-    counterIdIngredients = ArrIgridients.length - 6;
-    ArrIdIngredients.splice(6);
-  }
+  const countIgridients = ArrIgridients.length;
 
   let right = -16;
   let zIndex = 999;
 
-  const counterMoreIdIngredients: JSX.Element = (
-    <p className={`text text_type_digits-default ${styles.counter}`}>
-      +{counterIdIngredients}
-    </p>
-  );
-
   return (
     <ul className={styles.listIcon}>
-      {ArrIgridients.map(({ image_mobile, name, _id }, i) => (
+      {ArrIgridients.slice(0, 6).map(({ image_mobile, name, _id }, i) => (
         <li
-          className={styles.item}
+          className={cn(styles.item, { [styles.item__shadow]: i === 5 })}
           style={{ right: (right += 16), zIndex: (zIndex -= 1) }}
           key={_id + i}
         >
           <img className={styles.icon} src={image_mobile} alt={name} />
-          {i === 5 && counterIdIngredients ? counterMoreIdIngredients : null}
+          {i === 5 && countIgridients > 6 ? (
+            <p className={`text text_type_digits-default ${styles.counter}`}>
+              +{countIgridients - 6}
+            </p>
+          ) : null}
         </li>
       ))}
     </ul>
